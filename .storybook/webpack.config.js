f714d6449fa49c32fa932f23a24bf0228371f283
Daniel A.C. Martin <github@daniel-martin.co.uk>
2019-11-05 17:28:13 +0000

    Use preset for SCSS support

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,29 +1,9 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          './node_modules/style-loader/index.js',
-          { loader: './node_modules/css-loader/dist/cjs.js', options: { importLoaders: 1 } },
-          { loader: 'sass-loader' },
-        ]
-      },
-      {
-        test: /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/,
-        loader: './node_modules/file-loader/dist/cjs.js',
-        query: { name: 'static/media/[name].[hash:8].[ext]' }
-      },
-      {
-        test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
-        loader: './node_modules/url-loader/dist/cjs.js',
-        query: { limit: 10000, name: 'static/media/[name].[hash:8].[ext]' }
-      },
-      {
-        test: /\.stories\.(mjs|[jt]sx?)$/,
-        loaders: [require.resolve('@storybook/source-loader')],
-        enforce: 'pre'
-      }
-    ]
-  }
+module.exports = function({ config }) {
+  config.module.rules.push({
+    test: /\.stories\.(mjs|[jt]sx?)$/,
+    loaders: [require.resolve('@storybook/source-loader')],
+    enforce: 'pre'
+  });
+
+  return config;
 };
