dddf3af10fef3d4f8f393fb1fc7949fc52226341
Daniel A.C. Martin <github@daniel-martin.co.uk>
2019-11-25 21:02:43 +0000

    Fix: Correct name of test file

--- src/lib/components/button/index.spec.ts
+++ src/lib/components/button/index.spec.ts
@@ -1,18 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Button from './';
 
-it('contains the expected text', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    React.createElement(Button, {
+describe('Button', () => {
+  describe('when given a href', () => {
+    const button = h(Button, {
       href: '#',
       value: 'Go'
-    }),
-    div
-  );
+    });
+    const component = shallow(button);
 
-  expect(div.textContent).toEqual('Go');
+    it('is an anchor', () => expect(component.find('a').length).toEqual(1));
+    it('contains the expected text', () => expect(component.text()).toEqual('Go'));
+  });
 
-  ReactDOM.unmountComponentAtNode(div);
+  describe('when not given a href', () => {
+    describe('and given an onClick prop', () => {
+      const spy = jest.fn();
+      const button = h(Button, {
+        id: 'my-button',
+        onClick: spy,
+        value: 'Click me'
+      });
+      const component = shallow(button);
+
+      it('is an input[type=button]', () => expect(component.find('input[type="button"]').length).toEqual(1));
+      it('contains the expected text', () => expect(component.find('#my-button[value="Click me"]').length).toEqual(1));
+
+      describe('when clicked', () => {
+        component
+          .find('#my-button')
+          .simulate('click');
+
+        it('calls the onClick prop', () => expect(spy).toHaveBeenCalled());
+      });
+    });
+  });
 });
