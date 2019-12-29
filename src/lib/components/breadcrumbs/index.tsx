c0960c0069c4128d5e9e5f2d5293a1d760b134ef
Daniel A.C. Martin <github@daniel-martin.co.uk>
2019-12-29 13:28:22 +0000

    Implement Breadcrumbs component properly

--- src/lib/components/breadcrumbs/index.tsx
+++ src/lib/components/breadcrumbs/index.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react';
 
-const Breadcrumbs: React.SFC<any> = props => (
-  <nav className="breadcrumbs">
-    <ol>
-      <li><a href="#">Section</a></li>
-      <li><a href="#">Subsection</a></li>
-      <li>Subsection</li>
-    </ol>
-  </nav>
-);
+interface IItem {
+  /** The location to link to */
+  href: string,
+  /** The text of the item */
+  text: string,
+  /** The title of the link */
+  title?: string
+};
+
+interface IBreadcrumbsProps {
+  /** The list of links */
+  items: Array<IItem>
+};
+
+export const Breadcrumbs: React.SFC<IBreadcrumbsProps> = props => {
+  const current = props.items.pop();
+  const previous = props.items;
+
+  return (
+    <nav className="breadcrumbs">
+      <ol>
+        {previous.map((v, i) => (<li key={i}><a href={v.href} title={v.title}>{v.text}</a></li>))}
+        <li key={previous.length}>{current.text}</li>
+      </ol>
+    </nav>
+  );
+};
 
 export default Breadcrumbs;
