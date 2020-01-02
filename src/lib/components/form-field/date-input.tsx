8fcd6720a1d727c4590cc5d45f22fe3abe988d1f
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-01-02 21:29:16 +0000

    Add default hint to date-inputs

--- src/lib/components/form-field/date-input.tsx
+++ src/lib/components/form-field/date-input.tsx
@@ -1,55 +1,59 @@
 import * as React from 'react';
 import FormGroupWithFieldset from './form-group-with-fieldset';
 
-const DateInput: React.SFC<any> = props => (
-  <FormGroupWithFieldset
-    id={props.id}
-    className={props.className}
-    fieldsetClassName="date-input"
-    legend={props.label}
-    hint={props.hint}
-    error={props.error}
-  >
-    <div className="item">
-      <label htmlFor={`${props.id}-day`}>Day</label>
-      <input
-        id={`${props.id}-day`}
-        name={`${props.name}[day]`}
-        type="number"
-        pattern="[0-9]*"
-        className="width-2"
-        defaultValue={props.defaultValue && props.defaultValue.day}
-        disabled={props.disabled}
-        autoComplete={props.autoComplete && `${props.autoComplete}-day`}
-      />
-    </div>
-    <div className="item">
-      <label htmlFor={`${props.id}-month`}>Month</label>
-      <input
-        id={`${props.id}-month`}
-        name={`${props.name}[month]`}
-        type="number"
-        pattern="[0-9]*"
-        className="width-2"
-        defaultValue={props.defaultValue && props.defaultValue.month}
-        disabled={props.disabled}
-        autoComplete={props.autoComplete && `${props.autoComplete}-day`}
-      />
-    </div>
-    <div className="item">
-      <label htmlFor={`${props.id}-year`}>Year</label>
-      <input
-        id={`${props.id}-year`}
-        name={`${props.name}[year]`}
-        type="number"
-        pattern="[0-9]*"
-        className="width-4"
-        defaultValue={props.defaultValue && props.defaultValue.year}
-        disabled={props.disabled}
-        autoComplete={props.autoComplete && `${props.autoComplete}-day`}
-      />
-    </div>
-  </FormGroupWithFieldset>
-);
+const DateInput: React.SFC<any> = props => {
+  const hint = props.hint || 'For example, 12 11 2007';
+
+  return(
+    <FormGroupWithFieldset
+      id={props.id}
+      className={props.className}
+      fieldsetClassName="date-input"
+      legend={props.label}
+      hint={hint}
+      error={props.error}
+    >
+      <div className="item">
+        <label htmlFor={`${props.id}-day`}>Day</label>
+        <input
+          id={`${props.id}-day`}
+          name={`${props.name}[day]`}
+          type="number"
+          pattern="[0-9]*"
+          className="width-2"
+          defaultValue={props.defaultValue && props.defaultValue.day}
+          disabled={props.disabled}
+          autoComplete={props.autoComplete && `${props.autoComplete}-day`}
+        />
+      </div>
+      <div className="item">
+        <label htmlFor={`${props.id}-month`}>Month</label>
+        <input
+          id={`${props.id}-month`}
+          name={`${props.name}[month]`}
+          type="number"
+          pattern="[0-9]*"
+          className="width-2"
+          defaultValue={props.defaultValue && props.defaultValue.month}
+          disabled={props.disabled}
+          autoComplete={props.autoComplete && `${props.autoComplete}-day`}
+        />
+      </div>
+      <div className="item">
+        <label htmlFor={`${props.id}-year`}>Year</label>
+        <input
+          id={`${props.id}-year`}
+          name={`${props.name}[year]`}
+          type="number"
+          pattern="[0-9]*"
+          className="width-4"
+          defaultValue={props.defaultValue && props.defaultValue.year}
+          disabled={props.disabled}
+          autoComplete={props.autoComplete && `${props.autoComplete}-day`}
+        />
+      </div>
+    </FormGroupWithFieldset>
+  );
+};
 
 export default DateInput;
