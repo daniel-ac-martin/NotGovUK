1c8c0ef6ff7ff967c947f51ba99f4e5f91d3c07b
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-01-03 15:18:51 +0000

    Add DateInput component

---
+++ src/lib/components/date-input/README.md
@@ -0,0 +1,14 @@
+Date Input
+==========
+
+The [GDS Date Input component].
+
+
+Preview
+-------
+
+![Preview][Preview]
+
+
+[GDS Date Input component]: https://design-system.service.gov.uk/components/date-input/
+[Preview]: ../../__image_snapshots__/storyshots-itest-ts-image-storyshots-components-date-input-standard-1-snap.png
--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -2,6 +2,7 @@ export { default as BackLink } from './back-link';
 export { default as Breadcrumbs } from './breadcrumbs';
 export { default as Button, StartButton, SubmitButton } from './button';
 export { default as Checkboxes } from './checkboxes';
+export { default as DateInput } from './date-input';
 export { default as Details } from './details';
 export { default as FormField } from './form-field';
 export { default as Radios } from './radios';
