7cd32d12927442fefe278a775299d6e8d13859d8
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-01-03 17:10:50 +0000

    FormField: Spellcheck textareas by default

--- src/lib/components/form-field/index.ts
+++ src/lib/components/form-field/index.ts
@@ -76,7 +76,10 @@ export const FormField: React.SFC<IFormField> = props => {
     inline: (props.inline === null && (type === 'radios' && props.options && props.options.length <= 2)) || props.inline,
     rows: (props.rows === null && (type === 'textarea' && 5)) || props.rows,
     small: (props.small === null && (props.options && props.options.length >= 6)) || props.small,
-    spellcheck: (props.spellCheck !== null) && (props.spellCheck ? 'true' : 'false'),
+    spellcheck:
+      props.spellCheck !== null ? (props.spellCheck ? 'true' : 'false') :
+      type === 'textarea' ? 'true' :
+      undefined,
     type: type === 'native-date' ? 'date' : type
   };
 
