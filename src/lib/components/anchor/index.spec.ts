91bc0a3e21b40499825b8985feb20f8f4bcd20c1
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-01-14 20:30:03 +0000

    Anchor: Fix tests

--- src/lib/components/anchor/index.spec.ts
+++ src/lib/components/anchor/index.spec.ts
@@ -1,11 +1,17 @@
 import Anchor from './';
 
 describe('Anchor', () => {
-  describe('when given a href', () => {
-    const anchor = h(Anchor, { href: '/location' });
-    const component = shallow(anchor);
+  describe('when given a href with a relative URL', () => {
+    const component = shallow(h(Anchor, { href: '/location' }, 'My link'));
 
-    it('is a Link', () => expect(component.find('Link').length).toEqual(1));
-    it('is a link to the HRef provided', () => expect(component.find('Link[to="/location"]').length).toEqual(1));
+    it('is a Link', () => expect(component.find('NavLink').length).toEqual(1));
+    it('is a link to the HRef provided', () => expect(component.find('NavLink[to="/location"]').length).toEqual(1));
+  });
+
+  describe('when given a href with an absolute URL', () => {
+    const component = shallow(h(Anchor, { href: '//example.com/location' }, 'My link'));
+
+    it('is an anchor', () => expect(component.find('a').length).toEqual(1));
+    it('is an anchor to the HRef provided', () => expect(component.find('a[href="//example.com/location"]').length).toEqual(1));
   });
 });
