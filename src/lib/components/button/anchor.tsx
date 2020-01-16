c034bec438f1d39748097b1689d9adaec22bff69
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-01-16 13:04:26 +0000

    Import from closest location
    Alters some imports to pull from the closest possible location in order
    to prevent bootstrap problems.

--- src/lib/components/button/anchor.tsx
+++ src/lib/components/button/anchor.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { A } from '../..';
+import { A } from '../';
 
 const Anchor: React.SFC<any> = props => (
   <A id={props.id} className={props.className} href={props.href}>{props.text}</A>
