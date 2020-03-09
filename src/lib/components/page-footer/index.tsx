52f089cbef75aa52b3739ac5540f2a82189586a5
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-03-09 16:30:41 +0000

    PageFooter: Use govuk-frontend styling
    Also provides explicit support for wide displays.

--- src/lib/components/page-footer/index.tsx
+++ src/lib/components/page-footer/index.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
-import { className } from '../../helpers';
+import { bem, className } from '../../helpers';
 
 interface IPageFooter {
   /** Extra CSS classes to be applied */
   className?: string,
   /** HTML id */
-  id?: string
+  id?: string,
+  /** Whether to remove limits on width */
+  wide?: boolean
 };
 
 export const PageFooter: React.SFC<IPageFooter> = props => (
-  <footer id={props.id} className={className('page-footer', props.className)}>
-    <div className="inner">
+  <footer id={props.id} className={className('govuk-footer', props.className)} role="contentinfo">
+    <div className={bem('govuk-width-container', props.wide ? 'wide' : undefined)}>
       {props.children}
     </div>
   </footer>
@@ -18,7 +20,8 @@ export const PageFooter: React.SFC<IPageFooter> = props => (
 
 PageFooter.defaultProps = {
   className: null,
-  id: null
+  id: null,
+  wide: false
 };
 
 export default PageFooter;
