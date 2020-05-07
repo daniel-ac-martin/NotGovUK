3ba3adc86c1fb7ee173c0c1d1cacaaf7991924d4
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-05-07 15:33:43 +0100

    Server-rederer: Surface server errors
    Pipes any server errors through to the app so that they can be shown to
    the user.
    
    Also provides react-router with information on the status code.

--- packages/server-renderer/src/index.ts
+++ packages/server-renderer/src/index.ts
@@ -2,19 +2,51 @@ import { ComponentType, createElement as h } from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 
+const statusToTitle = {
+  400: 'Bad request',
+  401: 'Unauthorised',
+  402: 'Payment required',
+  403: 'Forbidden',
+  404: 'Page not found',
+  405: 'Method not allowed',
+  406: 'Not acceptable',
+  407: 'Proxy authentication required',
+  408: 'Request Timeout',
+  409: 'Conflict',
+  410: 'Gone',
+  418: 'I am a teapot',
+  500: 'Internal server error',
+  501: 'Not implemented',
+  502: 'Bad gateway',
+  503: 'Service unavailable',
+  504: 'Gateway timeout',
+  505: 'HTTP version not supported',
+};
+
 export const reactRenderer = <A extends object, B extends object>(App: ComponentType<A>, appProps: A, Template: ComponentType<B>, templateProps: B) => {
   const formatHTML = (req, res, body) => {
     const createApp = (App: ComponentType<A>, props: A) => (
       h(StaticRouter, {
         location: req.url,
         context: {
-          //status: res.statusCode
+          statusCode: res.statusCode
         },
       }, h(App, props))
     );
 
+    const err = (
+      body instanceof Error
+        ? {
+          statusCode: res.statusCode,
+          title: statusToTitle[res.statusCode] || 'Error',
+          message: body.message
+        }
+        : undefined
+    );
+
     const reqProps = {
-      title: body.toString()
+      err,
+      pageTitle: (err && err.title) || body.toString()
     };
     const fullAppProps = {
       ...appProps,
