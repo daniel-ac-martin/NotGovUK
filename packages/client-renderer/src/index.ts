474bbd94cc130c06bcf98f28351b681e75ef6fcf
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-05-18 13:39:03 +0100

    client-renderer: Consume app-composer package

--- packages/client-renderer/src/index.ts
+++ packages/client-renderer/src/index.ts
@@ -1,6 +1,50 @@
-import { hydrateOrRender } from './hydrate-or-render';
+import { ComponentType, createElement as h } from 'react';
+import { hydrate as originalHydrate, render as originalRender } from 'react-dom';
+import { ApplicationProps, ApplicationPropsCSR, ErrorPageProps, PageProps, PageLoader, compose } from '@not-govuk/app-composer';
+
+export const hydrateOrRender = (AppWrap: ComponentType<ApplicationProps>, PageWrap: ComponentType<PageProps>, ErrorPage: ComponentType<ErrorPageProps>, LoadingPage: ComponentType<PageProps>, pageLoader: PageLoader): void => {
+  interface IWindowWithHydration extends Window {
+    hydrationId?: string
+    hydrationProps?: ApplicationPropsCSR
+  };
+
+  const routerProps = {};
+  const windowWithProps: IWindowWithHydration = window;
+  const app = h(
+    compose({
+      AppWrap,
+      ErrorPage,
+      LoadingPage,
+      PageWrap,
+      pageLoader,
+      routerProps
+    }),
+    windowWithProps.hydrationProps
+  );
+  const root = document.getElementById(windowWithProps.hydrationId);
+
+  try {
+    originalHydrate(app, root);
+    console.info('Hydration complete.');
+  } catch (e) {
+    console.log(e);
+    console.warn('Hydration failed. Attempting to re-render...');
+    try {
+      originalRender(app, root);
+      console.info('Render complete.');
+    } catch (e) {
+      console.log(e);
+      console.warn('Render failed. Falling back to SSR...');
+    }
+  }
+
+  const jsEnabledClass = 'js-enabled';
+
+  document.body.className = (
+    document.body.className
+      ? `${document.body.className} ${jsEnabledClass}`
+      : jsEnabledClass
+  );
+};
 
 export default hydrateOrRender;
-export { hydrateOrRender };
-export { withPages } from './with-pages';
-export type { ErrorPageProps, PageWrapProps, Page, PageLoader, PageProps } from './with-pages';
