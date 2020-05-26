f2958cf637ec4fb195c1a1af246288a33c6c2dd1
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-05-26 18:48:07 +0100

    app-template: Simplify import of SCSS
    As we are now running webpack to build the server this line can be
    simplified.

--- packages/app-template/src/common/page-wrap.tsx
+++ packages/app-template/src/common/page-wrap.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Page as TPage } from '@not-govuk/app-composer';
 import { Page } from '@not-govuk/components';
 
-if (process.env.WEBPACK) require('./app.scss');
+import './app.scss';
 
 export const PageWrap: TPage = ({ routes, children }) => {
   const compare = (a, b) => (
