608a73282b4432d65c71fcf9bfb130e254a6a55a
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-05-28 23:18:56 +0100

    app-template: Split config file
    Let's have a common config file as well as one that is specific to the
    server. That way we have a place to dump config that all the 'wrap' and
    'page' components can draw on.

--- packages/app-template/src/server/config.ts
+++ packages/app-template/src/server/config.ts
@@ -1,14 +1,15 @@
 import { Mode, NodeEnv } from '@not-govuk/engine';
+import commonConfig from '../common/config';
 
-const config = {
+const serverConfig = {
+  ...commonConfig,
   env: process.env.NODE_ENV as NodeEnv,
   httpd: {
     host: process.env.LISTEN_HOST || '0.0.0.0',
     port: Number(process.env.LISTEN_PORT) || 8080
   },
   mode: (process.env.MODE || 'server') as Mode,
-  name: process.env.npm_package_name || 'my-app',
   ssrOnly: !!(process.env.SSR_ONLY && process.env.SSR_ONLY.match(/(yes|true)/i))
 };
 
-export default config;
+export default serverConfig;
