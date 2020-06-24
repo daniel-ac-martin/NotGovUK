ac8073850615e078dd2f15f22a3f79fe023e99b2
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-06-24 23:28:03 +0100

    Docs: Use new DocsPage component and add side nav
    Gathers all components in the project and populates a side navigation to
    allow people to click between them.
    
    Displays each component usins the new `DocsPage` component.

--- apps/govuk-docs/src/common/pages/components.tsx
+++ apps/govuk-docs/src/common/pages/components.tsx
@@ -1,32 +1,59 @@
 import { FC, Fragment, createElement as h } from 'react';
 import { PageProps } from '@not-govuk/app-composer';
-import { DocsContext } from '@not-govuk/docs-components';
+import { A } from '@not-govuk/components';
+import { DocsPage } from '@not-govuk/docs-components';
 
-import * as stories from '../../../../../components/my-component/spec/MyComponent.stories.mdx';
-
-const content = stories.default.parameters.docs.page();
-
-const storyNamesToSources = (acc: object, cur: string) => {
-  const story = stories[cur];
-  const name = story.storyName;
-  const source = story.parameters.storySource.source;
-
-  return { ...acc, [name]: source };
-};
-
-const contextValue = {
-  storySource: stories.default.includeStories.reduce(storyNamesToSources, {})
-};
+const reduceToLookup = (acc, cur) => ({...acc, [cur.default.title]: cur});
+const createSubpageStore = r => (
+  r
+    .keys()
+    .map(r)
+    .reduce(reduceToLookup, {})
+);
+const subpages = createSubpageStore(require.context('../../../../../components/', true, /\.stories\.mdx$/));
 
 const Page: FC<PageProps> = ({ location }) => {
+  const nameParam = 'name';
+  const componentName = location.query[nameParam];
+  const stories = subpages[componentName];
 
   return (
-    <Fragment>
-      <span className="govuk-caption-xl">Components</span>
-      <DocsContext.Provider value={contextValue}>
-        {content}
-      </DocsContext.Provider>
-    </Fragment>
+    <div className="govuk-grid-row">
+      <div className="govuk-grid-column-one-third">
+        <aside>
+          <h2>Components</h2>
+          <ul className="plain">
+            {Object.keys(subpages).map((v, i) => (
+              <li key={i}><A href={`/components?${nameParam}=${v}`}>{subpages[v].default.title}</A></li>
+            ))}
+          </ul>
+        </aside>
+      </div>
+      <div className="govuk-grid-column-two-thirds">
+        {
+          stories ? (
+            <Fragment>
+              <span className="govuk-caption-xl">Components</span>
+              <DocsPage stories={stories} />
+            </Fragment>
+          ) : (
+            componentName ? (
+              null // should be a 404!
+            ) : (
+              <Fragment>
+                <h1>Components</h1>
+                <p>
+                  Components are reusable parts of the user interface that have been made to support a variety of applications.
+                </p>
+                <p>
+                  Individual components can be used in multiple different patterns and contexts. For example, the text input component can be used to ask for an email address, a National Insurance number or someone’s name.
+                </p>
+              </Fragment>
+            )
+          )
+        }
+      </div>
+    </div>
   );
 };
 
