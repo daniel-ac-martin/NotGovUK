0221dabb198cd083972a05cfba176ae104a7f07f
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-06-26 19:13:59 +0100

    Netlify: New debug and serve static assets

--- netlify.sh
+++ netlify.sh
@@ -2,15 +2,8 @@
 
 set -euxo pipefail
 
-# Debug
-ls
-ls -l node_modules
-
 # Prepare for pnpm run
 npm install pnpm --no-progress --no-audit --no-fund --no-save --no-package-lock
-
-# Debug
-ls -l node_modules
 
 # Install npm dependencies
 ./node_modules/.bin/pnpm i --shamefully-hoist
@@ -19,10 +12,8 @@ ls -l node_modules
 # Build docs website
 cd ./apps/govuk-docs/
 npm run build
+du -h dist/
 cd ../../
 
 # Prepare node_modules/ for caching
-mv -v node_modules/.ignored/* node_modules/ || true
-ls -l apps/govuk-docs/node_modules
-ls -l node_modules
-ls -l node_modules/.pnpm
+mv node_modules/.ignored/* node_modules/ || true
