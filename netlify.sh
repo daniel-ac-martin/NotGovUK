d949dda51a966e776bd204df2c6eff96bac96264
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-06-26 15:27:59 +0100

    Netlify: Attempt to reinstate original node_modules/

--- netlify.sh
+++ netlify.sh
@@ -5,22 +5,23 @@ set -euxo pipefail
 # Debug
 ls
 ls node_modules
-ls apps/govuk-docs
-ls apps/govuk-docs/node_modules
 
-# Install pnpm
+# Prepare for pnpm run
+npm install pnpm --no-progress --no-audit --no-fund
 mkdir -p tools
-cd tools
-echo '{}' > package.json
-npm install pnpm
-cd ..
+mv node_modules/ tools/node_modules
+[ -f tools/node_modules/.pnpm-cache ] && mv tools/node_modules/.pnpm-cache node_modules || true
 
 # Install npm dependencies
 ./tools/node_modules/.bin/pnpm i
 
-# Clean-up tools
-rm -rf tools
-
 # Build docs website
 cd ./apps/govuk-docs/
 npm run build
+
+# Prepare node_modules/ for caching
+mv node_modules/ tools/node_modules/.pnpm-cache
+mv tools/node_modules/ node_modules
+
+# Clean-up
+rmdir tools
