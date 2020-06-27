2c9f5d8ab45f2cb2cb5d4ede3b76a5f4441b5d6c
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-06-27 18:44:13 +0100

    webpack-config: Make docs support optional
    The extra processing slows the build down a lot, the dependencies can
    be very large and only documentation website will need this
    functionality. So let's make this an opt-in feature.

--- apps/govuk-docs/webpack.config.js
+++ apps/govuk-docs/webpack.config.js
@@ -2,6 +2,7 @@ const generateConfig = require('@not-govuk/webpack-config');
 
 const webpackConfig = generateConfig({
   baseDir: __dirname,
+  docs: true,
   entry: './src/client/index.ts',
   outDir: './dist/public',
   production: process.env.NODE_ENV !== 'development',
--- apps/govuk-docs/webpack.config.server.js
+++ apps/govuk-docs/webpack.config.server.js
@@ -2,6 +2,7 @@ const generateConfig = require('@not-govuk/webpack-config');
 
 const webpackConfig = generateConfig({
   baseDir: __dirname,
+  docs: true,
   entry: './src/server/index.ts',
   outDir: './dist/server',
   production: process.env.NODE_ENV !== 'development',
