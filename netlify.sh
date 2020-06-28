edb92ea0a8cacb3b5dd085b4b7af730f0fcfe704
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-06-28 13:51:29 +0100

    Netlify: Remove unnecessary hack
    We shouldn't need to pretend that Vue is there anymore. If we do, that
    is a sign that something is wrong.

--- netlify.sh
+++ netlify.sh
@@ -40,9 +40,5 @@ rm -rf dist/servers/css/
 du -h dist/
 cd ../../
 
-# Dirty hack (fake vue package)
-mkdir -p node_modules/vue
-echo '{ "name": "vue", "version": "2.6.10" }' > node_modules/vue/package.json
-
 # Prepare node_modules/ for caching
 mv node_modules/.ignored/* node_modules/ || true
