01d60a06e73147d98221c752a28c717868fff824
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-07-06 18:32:52 +0100

    Netlify: Simplify script

--- netlify.sh
+++ netlify.sh
@@ -15,8 +15,7 @@ npm run build
 # Package functions
 ../../node_modules/.bin/sls package
 echo "process.env['MODE'] = 'serverless'; module.exports = require('./dist/server/index.js');" > govuk-docs.js
-zip -rv .serverless/govuk-docs.zip govuk-docs.js
-rm govuk-docs.js
+zip -mv .serverless/govuk-docs.zip govuk-docs.js
 
 # Arrange static assets
 mkdir public
