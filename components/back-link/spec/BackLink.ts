c3706a873caf5a306636e54807af0f77b78fb691
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-07-17 19:33:04 +0100

    BackLink: Fix tests

--- components/back-link/spec/BackLink.ts
+++ components/back-link/spec/BackLink.ts
@@ -8,18 +8,18 @@ describe('BackLink', () => {
       const backLink = h(BackLink, { href: '/back', text: 'Reverse' });
       const component = mount(backLink);
 
-      it('is a link', () => expect(component.find('A').length).toEqual(1));
-      it('is a link with the text provided', () =>expect(component.find('A[children="Reverse"]').length).toEqual(1));
-      it('links to the href provided', () => expect(component.find('A[href="/back"]').length).toEqual(1));
+      it('is a link', () => expect(component.find('a').length).toEqual(1));
+      it('is a link with the text provided', () =>expect(component.find('a[children="Reverse"]').length).toEqual(1));
+      it('links to the href provided', () => expect(component.find('a[href="/back"]').length).toEqual(1));
     });
 
     describe('but NOT a text property', () => {
       const backLink = h(BackLink, { href: '/back' });
       const component = mount(backLink);
 
-      it('is a link', () => expect(component.find('A').length).toEqual(1));
-      it('is a link with the text \'Back\'', () => expect(component.find('A[children="Back"]').length).toEqual(1));
-      it('links to the href provided', () => expect(component.find('A[href="/back"]').length).toEqual(1));
+      it('is a link', () => expect(component.find('a').length).toEqual(1));
+      it('is a link with the text \'Back\'', () => expect(component.find('a[children="Back"]').length).toEqual(1));
+      it('links to the href provided', () => expect(component.find('a[href="/back"]').length).toEqual(1));
     });
   });
 
