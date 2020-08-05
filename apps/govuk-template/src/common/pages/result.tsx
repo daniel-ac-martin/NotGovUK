03864c5adbad14472209ddeed414c8eea2bdcb20
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-08-05 12:36:59 +0100

    app-template: Code clean-up

--- apps/govuk-template/src/common/pages/result.tsx
+++ apps/govuk-template/src/common/pages/result.tsx
@@ -19,23 +19,25 @@ const Page: FC<PageProps> = props => {
   );
   const surname = data.name.split(' ').slice(-1)[0];
 
-  return (<Fragment>
-    <h1>Form complete</h1>
-    <p>
-      Hello {title} {surname}!
-    </p>
-    <div className="width-one-half" style={{ float: 'left' }}>
-      <h2>Result</h2>
-      <h3>GET</h3>
-      <pre>
-        {prettyPrint(location.query)}
-      </pre>
-      <h3>POST</h3>
-      <pre>
-        {prettyPrint(location.state)}
-      </pre>
-    </div>
-  </Fragment>);
+  return (
+    <Fragment>
+      <h1>Form complete</h1>
+      <p>
+        Hello {title} {surname}!
+      </p>
+      <div className="width-one-half" style={{ float: 'left' }}>
+        <h2>Result</h2>
+        <h3>GET</h3>
+        <pre>
+          {prettyPrint(location.query)}
+        </pre>
+        <h3>POST</h3>
+        <pre>
+          {prettyPrint(location.state)}
+        </pre>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Page;
