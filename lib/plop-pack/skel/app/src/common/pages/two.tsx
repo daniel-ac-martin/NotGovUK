203f06d8044636fbe5964bf9d81bcee152de5dfd
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-08-13 16:38:44 +0100

    plop-pack-internal: Remove extra page from app
    We probably don't need to provide more than one page so let's not.

--- lib/plop-pack/skel/app/src/common/pages/two.tsx
+++
@@ -1,7 +0,0 @@
-import { FC, createElement as h } from 'react';
-import { PageProps } from '@not-govuk/app-composer';
-
-const Page: FC<PageProps> = () => <h1>Page 2</h1>;
-
-export default Page;
-export const title = 'Two';
