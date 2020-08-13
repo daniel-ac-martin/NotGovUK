10c2b2e660e2a88c9f7367d60cafb6900f834d56
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-08-13 16:49:23 +0100

    Add GovUK-themed plop-pack package
    Adds a new plop-pack package that builds on the existing 'internal' one
    to add GovUK styling to the apps that it generates.

---
+++ lib-govuk/plop-pack/src/index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const { extendGenerator, relativePath } = require('@not-govuk/plop-pack-internal');
+
+const rel = relativePath(__dirname, '..', 'skel');
+
+const plopFunction = plop => {
+  const parent = '@not-govuk/plop-pack-internal';
+
+  plop.load('@not-govuk/plop-pack-internal', undefined, { actionTypes: true, generators: false, helpers: true, partials: false });
+
+  plop.setGenerator(
+    'app',
+    extendGenerator(plop, parent, 'app', {
+      prompts: [
+      ],
+      actions: [
+        {
+          type: 'copy',
+          destination: 'apps/{{{name}}}/src/',
+          source: rel('app/src/'),
+          overwrite: true
+        },
+        {
+          type: 'merge',
+          path: 'apps/{{{name}}}/package.json',
+          templateFile: rel('app/package.json.hbs')
+        }
+      ]
+    })
+  );
+
+  plop.setGenerator(
+    'component',
+    extendGenerator(plop, parent, 'component', {
+      prompts: [
+      ],
+      actions: [
+      ]
+    })
+  );
+
+  plop.setGenerator(
+    'lib',
+    extendGenerator(plop, parent, 'lib', {
+      prompts: [
+      ],
+      actions: [
+      ]
+    })
+  );
+
+  plop.setDefaultInclude({ actionTypes: true, generators: true, helpers: true });
+};
+
+module.exports = plopFunction;
