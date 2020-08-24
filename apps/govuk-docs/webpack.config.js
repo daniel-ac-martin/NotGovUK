8e3ee643a9a7cb880c59f65357a3ea620c506e43
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-08-24 23:10:22 +0100

    apps: Migrate to new webpack-config interface

--- apps/govuk-docs/webpack.config.js
+++ apps/govuk-docs/webpack.config.js
@@ -3,10 +3,11 @@ const generateConfig = require('@not-govuk/webpack-config');
 const webpackConfig = generateConfig({
   baseDir: __dirname,
   docs: true,
-  entry: './src/client/index.ts',
-  outDir: './dist/public',
-  production: process.env.NODE_ENV !== 'development',
-  tsConfig: 'tsconfig.json'
+  entry: {
+    client: './src/client/index.ts',
+    server: './src/server/index.ts'
+  },
+  outDir: './dist'
 });
 
 module.exports = webpackConfig;
