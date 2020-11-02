468a0ccf6bfcec3a4261d620a3e93b216249f30a
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-11-02 21:42:01 +0000

    govuk-template: Remove pointless page

--- apps/govuk-template/src/common/pages/two.tsx
+++
@@ -1,7 +0,0 @@
-import { FC, createElement as h } from 'react';
-import { PageProps } from '@not-govuk/app-composer';
-
-const Page: FC<PageProps> = () => <h1>Two</h1>;
-
-export default Page;
-export const title = 'Two';
