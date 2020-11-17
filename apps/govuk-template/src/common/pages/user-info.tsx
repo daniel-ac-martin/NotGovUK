2e666f925da2767a980f08e8cef5e8080ebe3e07
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-11-17 16:23:37 +0000

    govuk-template: Improve user-info demo page

--- apps/govuk-template/src/common/pages/user-info.tsx
+++ apps/govuk-template/src/common/pages/user-info.tsx
@@ -3,19 +3,28 @@ import { PageProps } from '@not-govuk/app-composer';
 import { useUserInfo } from '@not-govuk/user-info';
 
 const Page: FC<PageProps> = () => {
-  const { username, groups, roles } = useUserInfo();
+  const { username, groups, roles, displayName, name } = useUserInfo() || {};
+  const { givenName, familyName } = name || {};
 
   return (
     <Fragment>
       <h1>User Info</h1>
-      <dl>
-        <dt>Username</dt>
-        <dd>{username}</dd>
-        <dt>Groups</dt>
-        <dd>{groups?.join(', ')}</dd>
-        <dt>Roles</dt>
-        <dd>{roles?.join(', ')}</dd>
-      </dl>
+      { !username ? (<p>Not logged in.</p>) : (
+        <dl>
+          <dt>Username</dt>
+          <dd>{username}</dd>
+          <dt>Groups</dt>
+          <dd>{groups?.join(', ')}</dd>
+          <dt>Roles</dt>
+          <dd>{roles?.join(', ')}</dd>
+          <dt>Display name</dt>
+          <dd>{displayName}</dd>
+          <dt>Given name</dt>
+          <dd>{givenName}</dd>
+          <dt>Family name</dt>
+          <dd>{familyName}</dd>
+        </dl>
+      ) }
     </Fragment>
   );
 };
