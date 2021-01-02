e2dfa60908e310e263e0f373bea64ed544c0ffa9
Daniel A.C. Martin <github@daniel-martin.co.uk>
2021-01-02 18:43:20 +0000

    Form: Use and export new HOC

--- components/form/src/fields.ts
+++ components/form/src/fields.ts
@@ -1,4 +1,4 @@
-import { date, integer, minimum, range, withField } from '@not-govuk/forms';
+import { date, integer, minimum, range, withForm } from '@not-govuk/forms';
 import {
   Checkboxes as RawCheckboxes,
   DateInput as RawDateInput,
@@ -9,14 +9,14 @@ import {
   Textarea as RawTextarea
 } from '@not-govuk/components';
 
-export const Checkboxes: typeof RawCheckboxes = withField(RawCheckboxes);
-export const DateInput: typeof RawDateInput = withField(RawDateInput, [date()], {
+export const Checkboxes = withForm(RawCheckboxes);
+export const DateInput = withForm(RawDateInput, [date()], {
   day: [integer(), range(1, 31)('Enter a day between 1 and 31')],
   month: [integer(), range(1, 12)('Enter a month between 1 and 12')],
   year: [integer(), minimum(1000)('Enter a 4-digit year')]
 });
-export const Field: typeof RawField = withField(RawField);
-export const Radios: typeof RawRadios = withField(RawRadios);
-export const Select: typeof RawSelect = withField(RawSelect);
-export const TextInput: typeof RawTextInput = withField(RawTextInput);
-export const Textarea: typeof RawTextarea = withField(RawTextarea);
+export const Field = withForm(RawField);
+export const Radios = withForm(RawRadios);
+export const Select = withForm(RawSelect);
+export const TextInput = withForm(RawTextInput);
+export const Textarea = withForm(RawTextarea);
