7277984211bb887f88464cf58933721d4388a6c7
Daniel A.C. Martin <github@daniel-martin.co.uk>
2021-03-09 10:28:25 +0000

    Tabs: Make independent of internal tabs
    Going forward, the internal Tabs component will not rely work
    differently so we must decouple the two.

--- components/tabs/src/Tabs.tsx
+++ components/tabs/src/Tabs.tsx
@@ -1,17 +1,71 @@
-import { ComponentProps, FC, createElement as h } from 'react';
-import { Tabs as _Tabs } from '@not-govuk/tabs-internal';
+import { FC, ReactNode, createElement as h, useState } from 'react';
+import { StandardProps, classBuilder } from '@not-govuk/component-helpers';
 
 import '../assets/Tabs.scss';
 
-export type TabsProps = ComponentProps<typeof _Tabs>;
+type TabItem = {
+  id: string,
+  label: string,
+  content: ReactNode
+};
 
-export const Tabs: FC<TabsProps> = ({ classBlock, initial, items, ...props }) => (
-  <_Tabs
-    {...props}
-    classBlock={classBlock || 'govuk-tabs'}
-    items={items}
-    initial={initial || items[0]?.id}
-  />
-);
+export type TabsProps = StandardProps & {
+  items: TabItem[]
+  /** ID of item to show initially */
+  initial?: string
+  title?: string
+};
+
+export const Tabs: FC<TabsProps> = ({
+  children,
+  classBlock,
+  classModifiers,
+  className,
+  initial: _initial,
+  items,
+  title = 'Contents',
+  ...attrs
+}) => {
+  const classes = classBuilder('govuk-tabs', classBlock, classModifiers, className);
+  const initial = _initial || items[0]?.id;
+  const [ selected, setSelected ] = useState(initial);
+  const select = (id: string) => (e) => {
+    e.preventDefault();
+    setSelected(id === selected ? initial ? id : '' : id);
+  };
+  const ssr = !global.window;
+
+  return (
+    <div {...attrs} className={classes()}>
+      <h2 className={classes('title')}>{title}</h2>
+      <ul className={classes('list')} role="tablist">
+        { items.map(({ id, label }, i) => (
+          <li
+            key={i}
+            className={classes('list-item', id === selected ? 'selected' : undefined )}
+            role="presentation"
+            onClick={select(id)}
+          >
+            <a
+              aria-controls={id}
+              className={classes('tab')}
+              href={`#${id}`}
+              id={`tab_${id}`}
+              role="tab"
+              onClick={select(id)}
+            >
+              {label}
+            </a>
+          </li>
+        )) }
+      </ul>
+      { items.map(({ content, id, label, ...attrs2 }, i) => (
+        <div key={i} {...attrs2} className={classes('panel', ssr || id === selected ? undefined : 'hidden' )} id={id}>
+          {content}
+        </div>
+      )) }
+    </div>
+  );
+};
 
 export default Tabs;
