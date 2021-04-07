ad087987b01ff0f813b6e2f17714e200213ce539
Daniel A.C. Martin <github@daniel-martin.co.uk>
2021-04-07 21:47:49 +0100

    memoize: Accept more functions
    Accepts a greater variety of functions by using the `Map` object rather
    than a vanilla object to hold the memo.

--- lib/memoize/src/index.ts
+++ lib/memoize/src/index.ts
@@ -1,18 +1,20 @@
-type Key = string | number | symbol;
+type MemoizableFunction<A, B> = (v: A) => B;
 
-type MemoizableFunction<A extends Key, B> = (v: A) => B;
+export const memoize = <A, B>(f: MemoizableFunction<A, B>): MemoizableFunction<A, B> => {
+  const memo: Map<A, B> = new Map();
 
-export const memoize = <A extends Key, B>(f: MemoizableFunction<A, B>): MemoizableFunction<A, B> => {
-  const memo: Record<A, B> = {} as Record<A, B>;
+  return (a: A): B => {
+    const cached = memo.get(a);
 
-  return (v: A): B => {
-    const cached = memo[v];
+    if (cached === undefined) {
+      const result = f(a);
 
-    return (
-      cached === undefined
-        ? memo[v] = f(v)
-        : cached
-    );
+      memo.set(a, result);
+
+      return result;
+    } else {
+      return cached;
+    }
   };
 };
 
