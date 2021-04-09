e0bec6e21ae764a5ad3dd94659f941bd024c0909
Daniel A.C. Martin <github@daniel-martin.co.uk>
2021-04-09 16:06:01 +0100

    apps: Update the page-loader to accept HTML files
    We can now load in HTML files to create pages so let's enable his by
    default.

--- lib/plop-pack/skel/app/src/common/page-loader.ts
+++ lib/plop-pack/skel/app/src/common/page-loader.ts
@@ -1,2 +1,2 @@
-export const pageLoader = require.context('./pages', true, /\.[jt]sx?$/i, 'sync');
+export const pageLoader = require.context('./pages', true, /\.([jt]sx?|html)$/i, 'sync');
 export default pageLoader;
