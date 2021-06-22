06e981c6e2c659acfdab6177c1c8beacc59c8a10
Daniel A.C. Martin <github@daniel-martin.co.uk>
2021-06-22 23:01:57 +0100

    plop-pack-internal: Support defining site title

--- lib/plop-pack/skel/app/src/common/pages/index.tsx
+++ lib/plop-pack/skel/app/src/common/pages/index.tsx
@@ -1,6 +1,9 @@
 import { FC, Fragment, createElement as h } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { PageProps } from '@not-govuk/app-composer';
+import config from '../config';
+
+const siteTitle = config.title;
 
 export const title = 'Home';
 const description = 'Our homepage';
@@ -8,7 +11,7 @@ const description = 'Our homepage';
 const Page: FC<PageProps> = props => (
   <Fragment>
     <Helmet>
-      <title>{title}</title>
+      <title>{title} - {siteTitle}</title>
       <meta name="description" content={description} />
       <meta name="og:title" content={title} />
       <meta name="og:description" content={description} />
