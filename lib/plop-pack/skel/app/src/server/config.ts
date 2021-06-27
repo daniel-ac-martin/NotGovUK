c44ed160ff4175fa2e6889985c103591b9acb3cb
Daniel A.C. Martin <github@daniel-martin.co.uk>
2021-06-27 16:20:30 +0100

    plop-pack: Accept Heroku's PORT environment variable

--- lib/plop-pack/skel/app/src/server/config.ts
+++ lib/plop-pack/skel/app/src/server/config.ts
@@ -29,7 +29,7 @@ const serverConfig = {
   env,
   httpd: {
     host: process.env.LISTEN_HOST || '0.0.0.0',
-    port: Number(process.env.LISTEN_PORT) || 8080
+    port: Number(process.env.PORT) || Number(process.env.LISTEN_PORT) || 8080
   },
   mode: (process.env.MODE || 'server') as Mode,
   ssrOnly: !!(process.env.SSR_ONLY && process.env.SSR_ONLY.match(/(yes|true)/i))
