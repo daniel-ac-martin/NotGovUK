3e095139f42afa721ff317137d0eb2c93e0e8f20
Daniel A.C. Martin <github@daniel-martin.co.uk>
2021-08-17 22:19:13 +0100

    Form: Fix: Add explicit type declarations

--- components/form/src/fields.ts
+++ components/form/src/fields.ts
@@ -1,20 +1,21 @@
-import { date, integer, minimum, range, withForm } from '@not-govuk/forms';
-import { Checkboxes as _Checkboxes } from '@not-govuk/checkboxes';
-import { DateInput as _DateInput } from '@not-govuk/date-input';
-import { FormField as _Field } from '@not-govuk/form-field';
-import { Radios as _Radios } from '@not-govuk/radios';
-import { Select as _Select } from '@not-govuk/select';
-import { TextInput as _TextInput } from '@not-govuk/text-input';
-import { Textarea as _Textarea } from '@not-govuk/textarea';
+import { ComponentType } from 'react';
+import { FieldProps, date, integer, minimum, range, withForm } from '@not-govuk/forms';
+import { Checkboxes as _Checkboxes, CheckboxesProps } from '@not-govuk/checkboxes';
+import { DateInput as _DateInput, DateInputProps } from '@not-govuk/date-input';
+import { FormField as _Field, FormFieldProps } from '@not-govuk/form-field';
+import { Radios as _Radios, RadiosProps } from '@not-govuk/radios';
+import { Select as _Select, SelectProps } from '@not-govuk/select';
+import { TextInput as _TextInput, TextInputProps } from '@not-govuk/text-input';
+import { Textarea as _Textarea, TextareaProps } from '@not-govuk/textarea';
 
-export const Checkboxes = withForm(_Checkboxes);
-export const DateInput = withForm(_DateInput, [date()], {
+export const Checkboxes: ComponentType<CheckboxesProps & FieldProps> = withForm(_Checkboxes);
+export const DateInput: ComponentType<DateInputProps & FieldProps> = withForm(_DateInput, [date()], {
   day: [integer(), range(1, 31)('Enter a day between 1 and 31')],
   month: [integer(), range(1, 12)('Enter a month between 1 and 12')],
   year: [integer(), minimum(1000)('Enter a 4-digit year')]
 });
-export const Field = withForm(_Field as any);
-export const Radios = withForm(_Radios);
-export const Select = withForm(_Select);
-export const TextInput = withForm(_TextInput);
-export const Textarea = withForm(_Textarea);
+export const Field: ComponentType<FormFieldProps & FieldProps> = withForm(_Field as any);
+export const Radios: ComponentType<RadiosProps & FieldProps> = withForm(_Radios);
+export const Select: ComponentType<SelectProps & FieldProps> = withForm(_Select);
+export const TextInput: ComponentType<TextInputProps & FieldProps> = withForm(_TextInput);
+export const Textarea: ComponentType<TextareaProps & FieldProps> = withForm(_Textarea);
