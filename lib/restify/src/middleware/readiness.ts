cb80427772012556be622ea177f35d6ca499996e
Daniel A.C. Martin <github@daniel-martin.co.uk>
2022-02-19 13:32:11 +0000

    restify: Add support for readiness endpoint
    Takes an 'isReady' function that is expected to return true only when
    the application is able to serve traffic. (i.e. Has access to its
    dependencies.) When one is supplied, a readness endpoint is added to the
    server which can be utilised by orchestrators such as Kubernetes.

---
+++ lib/restify/src/middleware/readiness.ts
@@ -0,0 +1,16 @@
+import { Middleware } from './common';
+
+export type IsReady = () => boolean;
+
+export const readiness = (isReady: IsReady): Middleware => (_req, res, _next) => {
+  res.contentType = undefined;
+
+  if (isReady()) {
+    res.send('OK');
+  } else {
+    res.statusCode = 503;
+    res.send('Service Unavailable');
+  }
+};
+
+export default readiness;
