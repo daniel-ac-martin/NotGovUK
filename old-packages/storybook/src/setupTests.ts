d21b8332535f9fc1efb8a964cdf003334b42e891
Daniel A.C. Martin <github@daniel-martin.co.uk>
2022-03-12 23:34:08 +0000

    Delete old Storybook

--- old-packages/storybook/src/setupTests.ts
+++
@@ -1,9 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { h, mount, shallow } from './test-helpers';
-
-configure({ adapter: new Adapter() });
-
-global.h = h;
-global.mount = mount;
-global.shallow = shallow;
