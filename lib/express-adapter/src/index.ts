02d9c7e7cfeba1d4c64f76cfcc0994c9c22b9992
Daniel A.C. Martin <github@daniel-martin.co.uk>
2022-05-20 16:55:09 +0100

    express-adapter: Refactor to use Proxy
    Also adds extra functionality from asset-proxy and makes better use of
    Restify's API.

--- lib/express-adapter/src/index.ts
+++ lib/express-adapter/src/index.ts
@@ -1,29 +1,46 @@
-import type { Middleware as RestifyMiddleware } from '@not-govuk/restify';
+import etag from 'etag';
+
 import type { NextHandleFunction as ExpressMiddleware } from 'connect';
 import type { Response } from 'express-serve-static-core';
+import type { Middleware as RestifyMiddleware } from '@not-govuk/restify';
 
 export const adapt = (middleware: ExpressMiddleware): RestifyMiddleware => (req, res, next) => {
-  const res2: unknown = {
-    ...res,
-    getHeader(key) {
-      res.getHeader(key);
-    },
-    setHeader(key, val) {
-      res.setHeader(key, val);
-    },
-    send(content) {
-      res.send(content);
-      next();
-    },
-    end() {
-      res.statusCode = this.statusCode;
-      res.end();
-      next(false);
-    },
-    redirect(uri: string) { return res.redirect(uri, next) }
-  };
+  const expressRes: unknown = new Proxy(res, {
+    get(target, prop, receiver) {
+      switch (prop) {
+        case 'end':
+          return () => {
+            target.end();
+            return next(false);
+          }
+        case 'redirect':
+          return (uri: string) => {
+            return target.redirect(uri, next)
+          }
+        case 'send':
+          return (content) => {
+            target.statusCode = target.statusCode || 200;
 
-  return middleware(req, res2 as Response, next);
+            target.setHeader('Cache-Control', 'private, no-cache');
+
+            if (typeof content === 'string' || content instanceof Buffer) {
+              target.setHeader('ETag', etag(content, { weak: true }));
+              target.sendRaw(content);
+            } else {
+              target.send(content);
+            }
+
+            return next(false);
+          }
+      }
+
+      return Reflect.get(target, prop, receiver);
+    }
+  });
+
+  expressRes['locals'] = expressRes['locals'] || {};
+
+  return middleware(req, expressRes as Response, next);
 };
 
 export default adapt;
