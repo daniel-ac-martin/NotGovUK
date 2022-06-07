c0095f64e09b2767b9e1162bab3b3e3d5c58b89c
Daniel A.C. Martin <github@daniel-martin.co.uk>
2022-06-07 14:39:07 +0100

    template: Add beginnings of basic auth test

--- apps/govuk-template/feat/auth.spec.js
+++ apps/govuk-template/feat/auth.spec.js
@@ -1,9 +1,25 @@
 import config from './config.js';
 
-describe('Authentication', () => {
+const logInBasic = () => {
+  const { username, password } = config.auth.users.standard;
+
+  const encoded = (
+    Buffer
+      .from(username + ':' + password)
+      .toString('base64')
+  );
+
+  cy.visit('/auth/sign-in', {
+    headers: {
+      authorization: `Basic ${encoded}`
+    }
+  });
+};
+
+describe(`Authentication (${config.auth.method})`, () => {
   describe('when we have NOT logged in', () => {
     describe('and visit the user-info page', () => {
-      it('successfully loads', () => {
+      before(() => {
         cy.visit('/user-info');
       });
 
@@ -15,6 +31,7 @@ describe('Authentication', () => {
         it('shows we are logged in', () => {
           cy.contains('Username');
         });
+
         it('shows we are the dummy user', () => {
           cy.contains(config.auth.users.dummy.username);
         });
@@ -26,7 +43,32 @@ describe('Authentication', () => {
         it('shows we are NOT logged in', () => {
           cy.contains('Not logged in.');
         });
+
+        it('shows a sign-in link', () => {
+          cy.contains('Sign in').should('be.visible');
+        });
       }
     });
   });
+
+  if (config.auth.method === 'basic') {
+    describe.skip('when we log in with Basic auth',  () => {
+      before(() => {
+        logInBasic();
+        cy.contains('User Info').click();
+      });
+
+      it('shows we are logged in', () => {
+        cy.contains('Username');
+      });
+
+      it('shows we are the correct user', () => {
+        cy.contains(config.auth.users.standard.username);
+      });
+
+      it('shows we are have roles', () => {
+        cy.contains(config.auth.users.standard.roles[0]);
+      });
+    });
+  }
 });
