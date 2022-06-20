f2e1b0ceed34be6615ca6e58a446c03041e8b4e8
Daniel A.C. Martin <github@daniel-martin.co.uk>
2022-06-20 19:23:26 +0100

    restify: Stronger caching headers
    This should help prevent sensitive information from being cached as well
    as help ensure that stale data is not shown to the user.

--- lib/restify/src/middleware/no-cache-by-default.ts
+++ lib/restify/src/middleware/no-cache-by-default.ts
@@ -1,6 +1,8 @@
 function writeHead() {
   if (!this.getHeader('Cache-Control')) {
-    this.cache('no-cache, no-store');
+    this.cache('no-cache, no-store, must-revalidate, private');
+    this.header('Pragma', 'no-cache');
+    this.header('Expires', '0');
   }
 
   return this._restifyWriteHead.apply(this, arguments);
