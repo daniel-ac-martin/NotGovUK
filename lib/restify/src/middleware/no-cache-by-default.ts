503219f5c91af012767a5c9ec9ba470335d71850
Daniel A.C. Martin <github@daniel-martin.co.uk>
2022-06-21 14:51:32 +0100

    restify: Fix: Types in no-cache middleware

--- lib/restify/src/middleware/no-cache-by-default.ts
+++ lib/restify/src/middleware/no-cache-by-default.ts
@@ -1,17 +1,17 @@
 import type { Middleware } from "./common";
 
-function writeHead() {
-  if (!this.getHeader('Cache-Control')) {
-    this.cache('no-cache, no-store, must-revalidate, private');
-    this.header('Pragma', 'no-cache');
-    this.header('Expires', '0');
+export const noCacheByDefault: Middleware = (req, res, next) => {
+  const _writeHead = res.writeHead;
+  const writeHead = function () {
+    if (!this.getHeader('Cache-Control')) {
+      this.cache('no-cache, no-store, must-revalidate, private');
+      this.header('Pragma', 'no-cache');
+      this.header('Expires', '0');
+    }
+
+    return _writeHead.apply(this, arguments);
   }
 
-  return this._restifyWriteHead.apply(this, arguments);
-}
-
-export const noCacheByDefault: Middleware = (req, res, next) => {
-  res._restifyWriteHead = res.writeHead;
   res.writeHead = writeHead;
 
   next();
