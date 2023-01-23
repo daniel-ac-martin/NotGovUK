142f37d3e84dcecc3fec18774a2c54d6e7e0fe58
Daniel A.C. Martin <github@daniel-martin.co.uk>
2023-01-23 16:25:29 +0000

    restify: Add Permissions-Policy header
    This should further harden our HTTP responses and appease Zap.

---
+++ lib/restify/src/middleware/permissions-policy.ts
@@ -0,0 +1,78 @@
+import { Middleware } from './common';
+
+const keywords = [
+  'self',
+  'src'
+];
+
+const ppObj = {
+  'accelerometer': 'self',
+  'ambient-light-sensor': 'self',
+  'autoplay': 'self',
+  'battery': 'self',
+  'camera': 'self',
+  'display-capture': 'self',
+  'document-domain': 'self',
+  'encrypted-media ': 'self',
+  'execution-while-not-rendered': 'self',
+  'execution-while-out-of-viewport': 'self',
+  'fullscreen': 'self',
+  'gamepad': 'self',
+  'geolocation': 'self',
+  'gyroscope': 'self',
+  'hid': 'self',
+  'idle-detection': 'self',
+  'local-fonts': 'self',
+  'magnetometer': 'self',
+  'microphone': 'self',
+  'midi': 'self',
+  'payment': 'self',
+  'picture-in-picture': 'self',
+  'publickey-credentials-get': 'self',
+  'screen-wake-lock': 'self',
+  'serial': 'self',
+  'speaker-selection': 'self',
+  'usb': 'self',
+  'web-share': 'self',
+  'xr-spatial-tracking': 'self'
+};
+
+const isDefined = (v: any): boolean => (
+  v !== undefined
+);
+
+const policy = Object.keys(ppObj)
+  .map(directive => {
+    const _valueArr = ppObj[directive];
+    const valueArr = (
+      Array.isArray(_valueArr)
+        ? _valueArr
+        : [ _valueArr ]
+    );
+    const values = (
+      valueArr
+        .filter(isDefined)
+        .map(v => (
+          keywords.includes(v)
+            ? v
+            : `"${v}"`
+        ) )
+        .join(' ')
+    );
+
+    return (
+      values === ''
+      ? undefined
+        : `${directive}=(${values})`
+    );
+  } )
+  .filter(isDefined)
+  .join(', ');
+
+export const permissionsPolicy: Middleware = (_req, res, next) => {
+  res.header('Permissions-Policy', policy);
+
+  next();
+};
+
+export default permissionsPolicy;
