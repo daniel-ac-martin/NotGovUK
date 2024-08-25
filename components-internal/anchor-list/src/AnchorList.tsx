452d2bf082f1e485c4aba488612e6db07924cafe
Daniel A.C. Martin <github@daniel-martin.co.uk>
2024-08-25 19:52:00 +0100

    AnchorList: Avoid passing undefined

--- components-internal/anchor-list/src/AnchorList.tsx
+++ components-internal/anchor-list/src/AnchorList.tsx
@@ -30,7 +30,7 @@ export const AnchorList: FC<AnchorListProps> = ({
   const classes = classBuilder('penultimate-anchor-list', classBlock, classModifiers, className);
   const isActive = useActive();
   const processedItems = items.map(({ children, text, href, ...anchorAttrs }, i) => {
-    const active = isActive(href);
+    const active = isActive(href || '');
 
     return (
       <li key={i} className={classes('item', active ? 'active' : undefined)}>
