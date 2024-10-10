f0c708516a0a9fabc16bbadb7484dee8d7c4bf90
Daniel A.C. Martin <github@daniel-martin.co.uk>
2024-10-10 19:21:19 +0100

    next-example: Fix functional tests

--- apps/next-example/cypress.config.mjs
+++ apps/next-example/cypress.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from 'cypress';
 import projectConfig from '../../cypress.config.mjs';
 
 export default defineConfig({
-  ...projectConfig
+  ...projectConfig,
+  e2e: {
+    ...projectConfig.e2e,
+    baseUrl: 'http://localhost:3000'
+  }
 });
