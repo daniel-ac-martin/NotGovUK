6de4c62928861e544236e91215bfbf21ff19742d
Daniel A.C. Martin <github@daniel-martin.co.uk>
2024-11-03 17:35:04 +0000

    next-example: Add BackLink to a page

--- apps/next-example/src/app/one/two/page.tsx
+++ apps/next-example/src/app/one/two/page.tsx
@@ -1,8 +1,10 @@
+import { BackLink } from '@not-govuk/simple-components';
 const title = 'Two';
 
 export default function Page() {
   return (
     <>
+      <BackLink />
       <h1>
         {title}
       </h1>
