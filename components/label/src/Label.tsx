6ea139bc25db77e9fdfd3272d56f957ed6837329
Daniel A.C. Martin <github@daniel-martin.co.uk>
2024-12-10 20:50:32 +0000

    Allow labels, hints and errors to be hidden
    This is required for standalone inputs.

--- components/label/src/Label.tsx
+++ components/label/src/Label.tsx
@@ -5,13 +5,25 @@ import '../assets/Label.scss';
 
 export type LabelProps = StandardProps & LabelHTMLAttributes<HTMLLabelElement> & {
   children?: ReactNode
+  hidden?: boolean
 };
 
-export const Label: FC<LabelProps> = ({ children, classBlock, classModifiers, className, ...attrs }) => {
+export const Label: FC<LabelProps> = ({
+  children,
+  classBlock,
+  classModifiers: _classModifiers = [],
+  className,
+  hidden = false,
+  ...attrs
+}) => {
+  const classModifiers = [
+    hidden ? 'hidden' : undefined,
+    ...(Array.isArray(_classModifiers) ? _classModifiers : [_classModifiers])
+  ];
   const classes = classBuilder('govuk-label', classBlock, classModifiers, className);
 
   return (
-    <label {...attrs} className={classes()}>{children}</label>
+    <label {...attrs} className={classes()} aria-hidden={hidden}>{children}</label>
   );
 };
 
