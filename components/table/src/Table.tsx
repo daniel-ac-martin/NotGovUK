c66a9a83b920b8c4b0ea408a69ecd0cf8fcce917
Daniel A.C. Martin <github@daniel-martin.co.uk>
2025-03-17 14:42:37 +0000

    Table: Apply the `--m` caption class by default

--- components/table/src/Table.tsx
+++ components/table/src/Table.tsx
@@ -1,13 +1,31 @@
 import { ComponentProps, FC, createElement as h } from 'react';
-import { StandardProps, classBuilder } from '@not-govuk/component-helpers';
+import { classBuilder } from '@not-govuk/component-helpers';
 import { SimpleTable as _Table } from '@not-govuk/simple-table';
 
 import '../assets/Table.scss';
 
 export type TableProps = ComponentProps<typeof _Table>;
 
-export const Table: FC<TableProps> = ({ classBlock, ...props }) => (
-  <_Table {...props} classBlock={classBlock || 'govuk-table'} />
-);
+export const Table: FC<TableProps> = ({
+  classBlock: _classBlock,
+  classModifiers,
+  className,
+  caption: _caption,
+  ...props
+}) => {
+  const classBlock = _classBlock || 'govuk-table';
+  const classes = classBuilder('govuk-table', _classBlock, classModifiers, className);
+  const caption = (
+    typeof _caption !== 'string'
+      ? _caption
+      : (
+        <caption className={classes('caption', 'm')}>{_caption}</caption>
+      )
+  );
+
+  return (
+    <_Table {...props} classBlock={classBlock} classModifiers={classModifiers} className={className} caption={caption} />
+  );
+};
 
 export default Table;
