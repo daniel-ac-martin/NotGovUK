b94a2653bb145a5f3ef766fa041643904e247425
Daniel A.C. Martin <github@daniel-martin.co.uk>
2025-08-15 17:22:51 +0100

    remix-exmaple: Add conditional Radios example

--- apps/remix-example/app/routes/three.tsx
+++ apps/remix-example/app/routes/three.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Radios, TextInput } from '@not-govuk/components';
 
 const title = "Three";
 
@@ -16,6 +17,63 @@ export default function Page() {
         {title}
       </h1>
       <p>{title} content</p>
+      <p>Here is a conditional radios example:</p>
+      <Radios
+        label={
+          <h1 className="govuk-heading-l">
+            How would you prefer to be contacted?
+          </h1>
+        }
+        name="how-contacted"
+        options={[
+          {
+            value: "email",
+            label: "Email",
+            conditional: (
+              <TextInput
+                autoComplete="email"
+                className="govuk-!-width-one-third"
+                id="contact-by-email"
+                label="Email address"
+                name="contact-by-email"
+                spellCheck={false}
+                type="email"
+              />
+            ),
+          },
+          {
+            value: "phone",
+            label: "Phone",
+            conditional: (
+              <TextInput
+                autoComplete="tel"
+                className="govuk-!-width-one-third"
+                id="contact-by-phone"
+                label="Phone number"
+                name="contact-by-phone"
+                spellCheck={false}
+                type="tel"
+              />
+            ),
+          },
+          {
+            value: "text message",
+            label: "Text message",
+            conditional: (
+              <TextInput
+                autoComplete="tel"
+                className="govuk-!-width-one-third"
+                id="contact-by-text"
+                label="Mobile phone number"
+                name="contact-by-text"
+                spellCheck={false}
+                type="tel"
+              />
+            ),
+          },
+        ]}
+        hint="Select one option."
+      />
     </>
   );
 }
