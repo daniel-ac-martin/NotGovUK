a56484624f17c36eeb92315c78a4a65037e7ada9
Daniel A.C. Martin <github@daniel-martin.co.uk>
2025-08-16 15:17:42 +0100

    SummaryList: Simplify unit tests

--- components/summary-list/spec/SummaryList.ts
+++ components/summary-list/spec/SummaryList.ts
@@ -28,39 +28,6 @@ describe('SummaryList', () => {
     const props = {
       ...minimalProps,
       items: [
-        { name: 'Item A', actions: [ { href: '/a', text: 'Change A' } ], children: 'One' },
-        { name: 'Item B', actions: [ { href: '/b', text: 'Change B' } ], children: 'Two' },
-        { name: 'Item C', actions: [ { href: '/c', text: 'Change C' } ], children: 'Three' },
-      ]
-    };
-
-    beforeEach(async () => {
-      render(h(SummaryList, props));
-    });
-
-    it('contains the name of the 1st item', async () => expect(screen.getByText('Item A')).toBeInTheDocument());
-    it('contains the name of the 2nd item', async () => expect(screen.getByText('Item B')).toBeInTheDocument());
-    it('contains the name of the 3rd item', async () => expect(screen.getByText('Item C')).toBeInTheDocument());
-    it('contains the value of the 1st item', async () => expect(screen.getByText('One')).toBeInTheDocument());
-    it('contains the value of the 2nd item', async () => expect(screen.getByText('Two')).toBeInTheDocument());
-    it('contains the value of the 3rd item', async () => expect(screen.getByText('Three')).toBeInTheDocument());
-    it('represents all the actions as links', async () => expect(screen.getAllByRole('link')).toHaveLength(3));
-    it('contains the text of the 1st action', async () => expect(screen.getAllByRole('link')[0]).toHaveTextContent('Change A'));
-    it('contains the text of the 2nd action', async () => expect(screen.getAllByRole('link')[1]).toHaveTextContent('Change B'));
-    it('contains the text of the 3rd action', async () => expect(screen.getAllByRole('link')[2]).toHaveTextContent('Change C'));
-    it('links to the href of the 1st action', async () => expect(screen.getAllByRole('link')[0]).toHaveAttribute('href', '/a'));
-    it('links to the href of the 2nd action', async () => expect(screen.getAllByRole('link')[1]).toHaveAttribute('href', '/b'));
-    it('links to the href of the 3rd action', async () => expect(screen.getAllByRole('link')[2]).toHaveAttribute('href', '/c'));
-    it('renders 3 links', async () => expect(screen.queryAllByRole('link')).toHaveLength(3));
-    it('does not add the no-actions class modifier to the 1st item', async () => {
-      expect(screen.getByText('Item A').closest('div')).toHaveClass('govuk-summary-list__row');
-    })
-  });
-
-  describe('when given mixture of items with/without actions', () => {
-    const props = {
-      ...minimalProps,
-      items: [
         { name: 'Item A', children: 'One' },
         { name: 'Item B', actions: [ { href: '/b', text: 'Change B' } ], children: 'Two' },
         { name: 'Item C', actions: [ { href: '/c', text: 'Change C' } ], children: 'Three' },
@@ -91,5 +58,5 @@ describe('SummaryList', () => {
     it('does not add the no-actions class modifier to the 3rd item', async () => {
       expect(screen.getByText('Item A').closest('div')).toHaveClass('govuk-summary-list__row');
     })
-  })
+  });
 });
