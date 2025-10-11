e0567ffc1dc626fee22c4ba0b0dc9ff0a7ab5000
Daniel A.C. Martin <github@daniel-martin.co.uk>
2025-10-11 15:36:36 +0100

    Storybook: Clean up configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,43 +10,17 @@ const config: StorybookConfig = {
     '../styles/*.mdx'
   ],
   addons: [
-    // {
-    //   name: '@not-govuk/storybook-preset',
-    //   options: {
-    //     baseDir: __dirname,
-    //     tsConfig: '../tsconfig.storybook.json'
-    //   }
-    // },
     '@chromatic-com/storybook',
     '@storybook/addon-a11y',
-    // '@storybook/addon-actions', // DEFUNCT
     '@storybook/addon-docs',
-    // '@storybook/addon-jest', // BROKEN?
-    // '@storybook/addon-links', // No longer needed?
-    // '@storybook/addon-storysource', // DEFUNCT
-    // '@storybook/addon-viewport' // DEFUNCT
-    //'@storybook/addon-vitest' // Needs Jest->Vitest, does it have value?
   ],
   core: {
     disableTelemetry: true
   },
-  features: {
-    previewMdx2: true
-  },
   framework: {
     name: "@storybook/react-vite",
     options: {},
-  },
-  // webpackFinal: async (config) => {
-  //   config.module.rules.push({
-  //     test: /\.(stories|story)\.[tj]sx?$/,
-  //     loader: require.resolve('@storybook/source-loader'),
-  //     exclude: [/node_modules/],
-  //     enforce: 'pre'
-  //   });
-
-  //   return config;
-  // }
+  }
 };
 
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 import { createElement as h } from 'react';
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
-// import { withTests } from '@storybook/addon-jest';
 import globalDecorator from './decorators';
-// import jestResults from '../.jest-results.json';
 
 // FIXME: Temporary hack to get CSS
 import '@not-govuk/components/assets/index.scss';
@@ -11,10 +9,6 @@ import '@not-govuk/components/assets/index.scss';
 export const preview: Preview = {
   decorators: [
     globalDecorator,
-    // withTests({
-    //   results: jestResults,
-    //   filesExt: '((\\.specs?)|(\\.tests?))?(\\.(mjs|[jt]sx?))?$'
-    // })
   ],
   parameters: {
     a11y: {
