4a2fb354f2bcf69336b168aa1eaa9c7ca3d4056e
Daniel A.C. Martin <github@daniel-martin.co.uk>
2025-10-29 16:32:09 +0000

    fastify-dev-logger: Fix syntax in type definition

--- lib/fastify-dev-logger/src/index.d.ts
+++ lib/fastify-dev-logger/src/index.d.ts
@@ -3,6 +3,6 @@ import type { PrettyOptions, PrettyStream } from 'pino-pretty';
 export type FastifyDevLoggerOptions = Pick<PrettyOptions, 'append', 'destination', 'minimumLevel', 'mkdir', 'sync'> & {
 };
 
-declare export const fastifyDevLogger: (a: FastifyDevLoggerOptions): PrettyStream;
+declare export const fastifyDevLogger: (a: FastifyDevLoggerOptions) => PrettyStream;
 
 export default fastifyDevLogger;
