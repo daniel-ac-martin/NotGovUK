c82769f04ba3dbeb52060b71bd4cd70597b49d12
Daniel A.C. Martin <github@daniel-martin.co.uk>
2025-11-04 13:33:04 +0000

    docs: Enable middleware mode

--- apps/govuk-docs/react-router.config.ts
+++ apps/govuk-docs/react-router.config.ts
@@ -3,6 +3,9 @@ import type { Config } from "@react-router/dev/config";
 export default {
   appDirectory: 'src/app',
   buildDirectory: 'dist/app',
+  future: {
+    v8_middleware: true
+  },
   routeDiscovery: {
     mode: 'initial'
   },
