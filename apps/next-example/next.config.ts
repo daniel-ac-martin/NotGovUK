b1718e5c0ad19d64d2a9f7fbfbc67917a13959ed
Daniel A.C. Martin <github@daniel-martin.co.uk>
2025-11-11 14:32:05 +0000

    next-example: Upgrade to Next.js v16

---
+++ apps/next-example/next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from 'next';
+import webpack from 'webpack';
+
+const nextConfig: NextConfig = {
+  // This allows us to handle next-example, the same way we do standard apps, in CI
+  distDir: 'dist',
+  sassOptions: {
+    implementation: 'sass-embedded',
+    quietDeps: true, // Works around issues with govuk-frontend
+    silenceDeprecations: ['import'] // This is required until govuk-frontend moves to using modules
+  },
+  turbopack: {
+    resolveAlias: {
+      '@not-govuk/head': '@not-govuk/head/dummy',    // ADD THIS LINE
+      '@not-govuk/router': '@not-govuk/router/next', // ADD THIS LINE
+    }
+  }
+};
+
+export default nextConfig;
