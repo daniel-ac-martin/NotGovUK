585b4e18823f49078eab25219205eef41ca0d5f1
Daniel A.C. Martin <github@daniel-martin.co.uk>
2025-12-23 16:18:37 +0000

    next-example: Remove unnecessary import

--- apps/next-example/next.config.ts
+++ apps/next-example/next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from 'next';
-import webpack from 'webpack';
 
 const nextConfig: NextConfig = {
   // This allows us to handle next-example, the same way we do standard apps, in CI
