36811f4bdf3f522f5b99a3145e0acd48bbee2bc9
Daniel A.C. Martin <github@daniel-martin.co.uk>
2025-12-23 12:15:08 +0000

    Add fastify-consent-cookies package
    This is the spritual successor to the @not-govuk/consent-cookies
    package, which targets Restify / ExpressJS / Connect.

---
+++ lib/fastify-consent-cookies/src/common.ts
@@ -0,0 +1,34 @@
+import type { CookieSerializeOptions } from 'cookie';
+import type { FastifyInstance, FastifyRequest as _Request, FastifyReply as _Reply } from 'fastify';
+import type { Promised } from '@not-govuk/types-helpers';
+
+export type CookieOptions = Omit<CookieSerializeOptions, 'encode'>;
+
+export type SetCookie = (this: ReplyFull, name: string, value: any, options?: Omit<CookieOptions, 'httpOnly'>) => void;
+export type SetCookieConsent = (this: ReplyFull, value: string[]) => void;
+
+export type RequestExtras = {
+  cookies: Record<string, Cookie>
+  cookiesMeta: object
+};
+export type Request = _Request & Partial<RequestExtras>;
+export type RequestFull = Request & RequestExtras;
+
+type ReplyExtras = {
+  setCookie: SetCookie
+  setCookieConsent: SetCookieConsent
+};
+export type Reply = _Reply & Partial<ReplyExtras>;
+export type ReplyFull = _Reply & ReplyExtras;
+
+export type RouteHandlerMethod = (
+  this: FastifyInstance,
+  request: RequestFull,
+  reply: ReplyFull
+) => Promised<unknown | void>;
+
+export type Cookie = CookieOptions & {
+  name: string
+  description: string
+  group?: string
+};
