706e1ad020e1bba2be5373a8ce932adee7b74b88
Daniel A.C. Martin <github@daniel-martin.co.uk>
2026-01-03 13:49:40 +0000

    fastify: Fix defaultsTrue function

--- lib/fastify/src/config-helpers.ts
+++ lib/fastify/src/config-helpers.ts
@@ -12,10 +12,10 @@ export enum NodeEnv {
 type Pattern = RegExp | string;
 
 const parseBoolean = (pattern: Pattern) => (
-  (s?: string): boolean => !!(
+  (s: string): boolean => !!(
     s && s.match(pattern)
   )
 );
 
 export const defaultsFalse = parseBoolean(/(yes|on|true|1)/i);
-export const defaultsTrue = parseBoolean(/(no|off|false|0)/i);
+export const defaultsTrue = (s: string) => !parseBoolean(/(no|off|false|0)/i)(s);
