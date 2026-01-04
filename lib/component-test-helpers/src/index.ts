22df13129be050f12237a3b9fc1080680344a2fb
Daniel A.C. Martin <github@daniel-martin.co.uk>
2026-01-04 17:34:04 +0000

    component-test-helpers: Remove Helmet provider
    Remove the Helmet provider as we no longer need it after migrating to
    React v19.

--- lib/component-test-helpers/src/index.ts
+++ lib/component-test-helpers/src/index.ts
@@ -1,27 +1,22 @@
 import { FC, ReactElement, ReactNode, createElement as h } from 'react';
-import reactHelmetDefault, * as reactHelmetNamed from 'react-helmet-async';
 import { MemoryRouter } from 'react-router';
 import { render as _render, RenderOptions } from '@testing-library/react';
 import userEventDefault from '@testing-library/user-event';
 
 import '@testing-library/jest-dom';
 
-const reactHelmet = reactHelmetDefault || reactHelmetNamed;
-const { HelmetProvider } = reactHelmet;
-
 const Providers: FC<{ children?: ReactNode, routerProps?: object }> = ({
   children,
   routerProps
 }) => (
-  h(HelmetProvider, {},
-    h(MemoryRouter, routerProps || {
-      future: {
-        v7_relativeSplatPath: true,
-        v7_startTransition: true
-      },
-      initialEntries: ['/previous', '/current', '/next'],
-      initialIndex: 1
-    }, children) )
+  h(MemoryRouter, routerProps || {
+    future: {
+      v7_relativeSplatPath: true,
+      v7_startTransition: true
+    },
+    initialEntries: ['/previous', '/current', '/next'],
+    initialIndex: 1
+  }, children)
 );
 
 export const render = (
