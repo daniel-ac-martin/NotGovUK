64306ab086a696225cefc4fc117fcaaf35ae8245
Daniel A.C. Martin <github@daniel-martin.co.uk>
2026-08-06 17:11:37 +0100

    Page: Update assets to new crown
    This also aligns the not-govuk page more closely to the current govuk
    page.

--- components/page/src/NotGovUKPage.tsx
+++ components/page/src/NotGovUKPage.tsx
@@ -6,17 +6,15 @@ import { Page, PageProps } from './Page';
 import { unwrapImage } from './types';
 
 import iFavicon from '../assets/coat-favicon.ico';
-import iAppleTouchIcon180 from '../assets/coat-apple-touch-icon-180x180.png';
-import iAppleTouchIcon167 from '../assets/coat-apple-touch-icon-167x167.png';
-import iAppleTouchIcon152 from '../assets/coat-apple-touch-icon-152x152.png';
-import iAppleTouchIcon from '../assets/coat-apple-touch-icon.png';
+import iFaviconSVG from '../assets/coat-favicon.svg';
+import iMaskIcon from '../assets/coat-icon-mask.svg';
+import iAppleTouchIcon180 from '../assets/coat-icon-180.png';
 import iOGImage from '../assets/coat-opengraph-image.png';
 
 const favicon = unwrapImage(iFavicon);
+const faviconSVG = unwrapImage(iFaviconSVG);
+const maskIcon = unwrapImage(iMaskIcon);
 const appleTouchIcon180 = unwrapImage(iAppleTouchIcon180);
-const appleTouchIcon167 = unwrapImage(iAppleTouchIcon167);
-const appleTouchIcon152 = unwrapImage(iAppleTouchIcon152);
-const appleTouchIcon = unwrapImage(iAppleTouchIcon);
 const ogImage = unwrapImage(iOGImage);
 
 import '../assets/NotGovUKPage.scss';
@@ -31,11 +29,10 @@ export const NotGovUKPage: FC<NotGovUKPageProps> = ({ children, classModifiers, 
     govUK={false}
   >
     <meta name="theme-color" content={'#0b0c0c'} />
-    <link rel="shortcut icon" sizes="16x16 32x32 48x48" href={favicon} type="image/x-icon" />
-    <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon180} />
-    <link rel="apple-touch-icon" sizes="167x167" href={appleTouchIcon167} />
-    <link rel="apple-touch-icon" sizes="152x152" href={appleTouchIcon152} />
-    <link rel="apple-touch-icon" href={appleTouchIcon} />
+    <link rel="icon" sizes="16x16 32x32 48x48" href={favicon} />
+    <link rel="icon" sizes="any" href={faviconSVG} type="image/svg+xml" />
+    <link rel="mask-icon" href={maskIcon} color={'#0b0c0c'} />
+    <link rel="apple-touch-icon" href={appleTouchIcon180} />
     <meta property="og:image" content={ogImage} />
     {children}
   </Page>
