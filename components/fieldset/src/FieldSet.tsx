cd40648928b5c797a59a60c6edcdf521eae74854
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2026-09-23 17:21:05 +0100

    FieldSet: Add legendSize and isPageHeading props

--- components/fieldset/src/FieldSet.tsx
+++ components/fieldset/src/FieldSet.tsx
@@ -3,9 +3,15 @@ import { StandardProps, classBuilder } from '@react-foundry/component-helpers';
 
 import '../assets/FieldSet.scss';
 
+export type LegendSize = 's' | 'm' | 'l' | 'xl';
+
 export type FieldSetProps = StandardProps & FieldsetHTMLAttributes<HTMLFieldSetElement> & {
   children?: ReactNode
+  /** Whether the legend is the page heading (wraps it in an h1) */
+  isPageHeading?: boolean
   legend: ReactNode
+  /** Size of the legend text */
+  legendSize?: LegendSize
 };
 
 export const FieldSet: FC<FieldSetProps> = ({
@@ -13,14 +19,18 @@ export const FieldSet: FC<FieldSetProps> = ({
   classBlock,
   classModifiers,
   className,
+  isPageHeading = false,
   legend,
+  legendSize,
   ...attrs
 }) => {
   const classes = classBuilder('govuk-fieldset', classBlock, classModifiers, className);
 
   return (
     <fieldset {...attrs} className={classes()}>
-      <legend className={classes('legend')}>{legend}</legend>
+      <legend className={classes('legend', legendSize)}>
+        { isPageHeading ? <h1 className={classes('heading')}>{legend}</h1> : legend }
+      </legend>
       {children}
     </fieldset>
   );
